80338c8b6a576feecd4b8764a944bdcb6ab31c8a
Mr-Jason-Sam <Mr.Jason_Sam@iCloud.com>
2017-04-17 23:25:10 +0800

    queue_start
    queue_start

---
+++ Algorithms/Algorithms/Queue.hpp
@@ -0,0 +1,14 @@
+//
+//  Queue.hpp
+//  Algorithms
+//
+//  Created by Jason_Sam on 2017/4/16.
+//  Copyright © 2017年 Jason_Sam. All rights reserved.
+//
+
+#ifndef Queue_hpp
+#define Queue_hpp
+
+#include <stdio.h>
+
+#endif /* Queue_hpp */
