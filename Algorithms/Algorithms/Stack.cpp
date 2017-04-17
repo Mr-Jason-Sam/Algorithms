a2ad34df361e871c6efa447c0446465fa491265e
Mr-Jason-Sam <Mr.Jason_Sam@iCloud.com>
2017-04-17 23:24:52 +0800

    stack_start
    stack_start

---
+++ Algorithms/Algorithms/Stack.cpp
@@ -0,0 +1,9 @@
+//
+//  Stack.cpp
+//  Algorithms
+//
+//  Created by Jason_Sam on 2017/4/16.
+//  Copyright © 2017年 Jason_Sam. All rights reserved.
+//
+
+#include "Stack.hpp"
---
+++ Algorithms/Algorithms/Stack.hpp
@@ -0,0 +1,14 @@
+//
+//  Stack.hpp
+//  Algorithms
+//
+//  Created by Jason_Sam on 2017/4/16.
+//  Copyright © 2017年 Jason_Sam. All rights reserved.
+//
+
+#ifndef Stack_hpp
+#define Stack_hpp
+
+#include <stdio.h>
+
+#endif /* Stack_hpp */
