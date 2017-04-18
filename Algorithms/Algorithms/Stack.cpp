1cbe0b2b2fd703bcbe0742392381c0954b1ba98d
Mr-Jason-Sam <Mr.Jason_Sam@iCloud.com>
2017-04-18 23:25:24 +0800

    stack
    Stack

--- Algorithms/Algorithms/Stack.cpp
+++ Algorithms/Algorithms/Stack.cpp
@@ -7,3 +7,104 @@
 //
 
 #include "Stack.hpp"
+#include "List.hpp"
+
+int stack_create(Stack **stack){
+    if ((*stack = (Stack*)malloc(sizeof(Stack))) == NULL) {
+        return -1;
+    }
+    (*stack)->top  = 0;
+    return 0;
+}
+bool stack_empty(Stack *stack){
+    return stack->top == 0;
+}
+bool stack_full(Stack *stack){
+    return stack->top == MAX;
+}
+int stack_pop(Stack *stack){
+    if (stack_empty(stack)) {
+        return -1;
+    }
+    stack->top--;
+    return 0;
+}
+int stack_push(Stack *stack, const int data){
+    if (stack_full(stack)) {
+        return -1;
+    }
+    stack->data[stack->top++] = data;
+    return 0;
+}
+int stack_gettop(Stack *stack){
+    return stack->data[stack->top-1];
+}
+
+/*--------------link stack------------------*/
+
+int link_stack_create(SLink **link){
+    if ((*link = (SLink*)malloc(sizeof(SLink))) == NULL) {
+        return -1;
+    }
+    (*link)->size = 0;
+    (*link)->head= NULL;
+    return 0;
+}
+
+int link_stack_pop(SLink *link){
+    LSElem *old_elem;
+    if (link->head == NULL) {
+        return -1;
+    }
+    int data = link->head->data;
+    old_elem = link->head;
+    if(link->head->next != NULL)
+        link->head = link->head->next;
+    free(old_elem);
+    return data;
+}
+int link_stack_push(SLink *link, const int data){
+    LSElem *new_elem;
+    if ((new_elem = (LSElem*)malloc(sizeof(LSElem))) == NULL) {
+        return -1;
+    }
+    new_elem->data = data;
+    if (link->head != NULL) {
+        new_elem->next = link->head;
+    }
+    link->head = new_elem;
+    return 0;
+}
+int link_stack_gettop(SLink *link){
+    return link->head->data;
+}
+
+/*--------------link stack------------------*/
+
+void excute_stack(){
+    Stack *elem = NULL;
+    stack_create(&elem);
+    for (int i =0; i < MAX; i++) {
+        stack_push(elem,i);
+    }
+    cout << stack_gettop(elem);
+    stack_pop(elem);
+    cout << stack_gettop(elem);
+    stack_pop(elem);
+    cout << stack_gettop(elem);
+    cout <<endl;
+    
+//    SLink *list = NULL;
+//    link_stack_create(&list);
+//    for (int i =0; i < MAX; i++) {
+//        link_stack_push(list, i);
+//    }
+//    cout << link_stack_gettop(list);
+//    link_stack_pop(list);
+//    cout << link_stack_gettop(list);
+//    link_stack_pop(list);
+//    cout << link_stack_gettop(list);
+//    cout << endl;
+    
+}
+
--- Algorithms/Algorithms/Stack.hpp
+++ Algorithms/Algorithms/Stack.hpp
@@ -10,5 +10,43 @@
 #define Stack_hpp
 
 #include <stdio.h>
+#include <iostream>
+#define MAX 5
+using namespace std;
+
+
+typedef struct Stack_{
+    int data[MAX];
+    int top;
+}Stack;
+
+int stack_create(Stack **stack);
+bool stack_empty(Stack *stack);
+bool stack_full(Stack *stack);
+int stack_pop(Stack *stack);
+int stack_push(Stack *stack, const int data);
+int stack_gettop(Stack *stack);
+
+/*--------------link stack------------------*/
+typedef struct LSElem_{
+    int data;
+    struct LSElem_ *next;
+}LSElem;
+
+typedef struct SLink_{
+    int size;
+    LSElem *head;
+}SLink;
+
+int link_stack_create(SLink **link);
+int link_stack_pop(SLink *top);
+int link_stack_push(SLink *top, const int data);
+int link_stack_gettop(SLink *top);
+
+/*--------------link stack------------------*/
+
+void excute_stack();
 
 #endif /* Stack_hpp */
+
+
